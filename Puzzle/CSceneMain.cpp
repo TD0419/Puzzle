b317edea1c6dc5ff7c5636ea98ebff0f8980b774
津田 直志 <h1201616001@hiratagakuen.onmicrosoft.com>
2019-03-06 11:07:05 +0900

    田中先生のライブラリ追加

---
+++ Puzzle/CSceneMain.cpp
@@ -0,0 +1,38 @@
+//STLデバック機能をOFFする
+#define _SECURE_SCL (0)
+#define _HAS_IETRATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawTexture.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//使用ヘッダー
+#include "CSceneMain.h"
+#include "GameHead.h"
+
+//コンストラクタ
+CSceneMain::CSceneMain()
+{
+
+}
+
+//デストラクタ
+CSceneMain::~CSceneMain()
+{
+	
+}
+
+//初期化メソッド
+void CSceneMain::InitScene()
+{
+	
+}
+
+//実行中メソッド
+void CSceneMain::Scene()
+{
+	
+}
